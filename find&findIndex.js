915cd83bb1281c6eb48258ab216f8e905504cf6a
Vishal <53686943+am-vishal@users.noreply.github.com>
2021-02-20 12:45:07 +0530

    Create find&findIndex.js

---
+++ find&findIndex.js
@@ -0,0 +1,9 @@
+//find
+let numbers = [4, 9, 16, 25, 29];
+let first = numbers.find((value => value > 20));
+console.log(first)
+
+//findIndex
+let numbers = [4, 9, 16, 25, 29];
+let first = numbers.findIndex((value => value > 20));
+console.log(first)
