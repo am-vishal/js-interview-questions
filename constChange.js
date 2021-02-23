620fb705a9d9d9f89c2d4ea366010081c29e1225
Vishal <53686943+am-vishal@users.noreply.github.com>
2021-02-23 23:11:44 +0530

    Update constChange.js

--- constChange.js
+++ constChange.js
@@ -2,3 +2,17 @@
 const obj = {name:"vishal"};
 obj.name = "false"
 console.log(obj.name)
+
+//Redeclare const value
+const toto = 42;
+const foo = "bar";
+console.log(toto);
+			
+//Some added script
+{
+    const toto = "hello world !";
+    console.log(toto);
+    console.log(foo);
+}
+//back to main script
+console.log(toto);
