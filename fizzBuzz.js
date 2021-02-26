9a017b33e92bfdce3447ee98922a8fd799193365
Vishal <53686943+am-vishal@users.noreply.github.com>
2021-02-27 00:19:30 +0530

    Create fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+//Example:1
+// function fizzBuzz2(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+
+//     if (i % 3 === 0) str += "fizz"
+//     if (i % 5 === 0) str += "buzz"
+//     if (str === "") str = i;
+  
+//     console.log(str);
+//   }
+// }
+// fizzBuzz2(25)
