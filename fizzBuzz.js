487f4353b82879b729cb8a68e439ebb9f882f7ea
Vishal <53686943+am-vishal@users.noreply.github.com>
2021-02-27 00:21:37 +0530

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,26 @@
 //Example:1
-// function fizzBuzz2(n) {
-//   for (let i = 1; i <= n; i++) {
-//     let str = "";
+function fizzBuzz2(n) {
+  for (let i = 1; i <= n; i++) {
+    let str = "";
 
-//     if (i % 3 === 0) str += "fizz"
-//     if (i % 5 === 0) str += "buzz"
-//     if (str === "") str = i;
+    if (i % 3 === 0) str += "fizz"
+    if (i % 5 === 0) str += "buzz"
+    if (str === "") str = i;
   
-//     console.log(str);
-//   }
-// }
-// fizzBuzz2(25)
+    console.log(str);
+  }
+}
+ fizzBuzz2(25)
+
+//Example:2
+for (let i=1; i <= 25; i++)
+{
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
