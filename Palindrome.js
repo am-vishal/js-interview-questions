11bcf568288b53e1891de1ba323d9178b959fd4a
Vishal <53686943+am-vishal@users.noreply.github.com>
2021-03-05 23:55:12 +0530

    Update Palindrome.js

--- Palindrome.js
+++ Palindrome.js
@@ -5,3 +5,19 @@ function isPalindrome(str) {
 }
 
 isPalindrome(str); // return true
+
+//Example:2
+let string = "t  E ne T ";
+let regex = /[^a-z0-9]/g;
+function stringForLoop(str) {
+  str = str.toLowerCase().replace(regex, '');
+  let len = str.length;  
+  for (let i = 0; i < len; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+  }
+  return true;
+}
+console.log('is palindrom',stringForLoop(string));
+//Output: is palindrom True
