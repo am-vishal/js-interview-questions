dcaf483a55c2c2a142f1c7414c531f69f912d77a
Vishal <53686943+am-vishal@users.noreply.github.com>
2021-03-05 23:58:55 +0530

    Update findDuplicateInArr.js

--- findDuplicateInArr.js
+++ findDuplicateInArr.js
@@ -1,4 +1,5 @@
- let arr =  [41,1,2,3,4,'musk',1,2];
+//Example:1 
+let arr =  [41,1,2,3,4,'musk',1,2];
   let commonElement = arr.sort(); 
   var results = [];
 
@@ -8,10 +9,10 @@
     }
   }
 console.log(results);
-
 //Output: [1, 2]
 
+//Example:2
+// A value in the Set may only occur once; it is unique in the Set's collection.
 const arr = [1,2,3,4,4,7,5,6,4,5,6];
 console.log([...new Set(arr)])
-
 //Output: [ 1, 2, 3, 4, 7, 5, 6 ]
